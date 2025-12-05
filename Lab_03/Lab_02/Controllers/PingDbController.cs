05e5294896275a676d2678325d4daadf9dac28b7
huannt0608-ops <huannt0608@gmail.com>
2025-12-06 01:36:00 +0700

    done Lab_03

---
+++ Lab_03/Lab_02/Controllers/PingDbController.cs
@@ -0,0 +1,23 @@
+﻿using Lab_02.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_02.Controllers
+{
+    [Route("ping-db")]
+    public class PingDbController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PingDbController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Ping()
+        {
+            bool ok = await _context.Database.CanConnectAsync();
+            return Content(ok ? "OK" : "FAIL");
+        }
+    }
+}
---
+++ Lab_03/Lab_02/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Lab_02.Models;
+
+namespace Lab_02.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
+}
