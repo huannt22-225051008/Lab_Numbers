17b5e4828f89bb132074d01f3f70dd295c83984d
huannt0608-ops <huannt0608@gmail.com>
2025-12-06 01:34:19 +0700

    done Lab_04

---
+++ Lab_04/Lab_02/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //GET: /HelloWorld/
+
+        //GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+
+            ViewData["Message"] = "Hello " + name;
+
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
